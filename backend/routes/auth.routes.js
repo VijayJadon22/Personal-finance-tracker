b62512cc94d42cbd724b49f99cd379a752a1f1f6
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-03 15:33:14 +0530

    Added authMiddleware is get request to getUser

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getUser, loginUser, logoutUser, signupUser } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 //GET request to get the user
-router.get("/", getUser);
+router.get("/", authMiddleware, getUser);
 
 //POST routes for user authentication
 router.post("/signup", signupUser);
