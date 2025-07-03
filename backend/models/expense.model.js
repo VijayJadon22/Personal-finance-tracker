361072659758422ace5fdfd717c18dfdd39dcf63
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-03 15:43:32 +0530

    Defined expenseSchema and created Expense Model for doing CRUD of user expenses

---
+++ backend/models/expense.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const expenseSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: [1, "Amount must be a positive number"]
+    },
+    category: {
+        type: String,
+        enum: ["Food", "Rent", "Shopping", "Utilities", "Travel", "Other"],
+        required: true,
+        trim: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    paymentMethod: {
+        type: String,
+        enum: ["UPI", "Credit Card", "Cash", "Debit Card", "Net Banking", "Other"],
+        required: true,
+    },
+    notes: {
+        type: String,
+        default: ""
+    }
+}, { timestamps: true });
+
+const Expense = mongoose.model("Expense", expenseSchema);
+export default Expense;
