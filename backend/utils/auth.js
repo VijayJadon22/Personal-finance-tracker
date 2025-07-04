4193922cd48ce22fb2b4099f43fd7c1d49a2bcb8
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-04 16:49:19 +0530

    Changed sameSite from stcrict to none as backend and front are deployed on different sites

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -4,7 +4,7 @@ export const generateTokenAndSetCookies = async (user, res) => {
 
     const options = {
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000 //7 days
     }
     res.cookie("token", token, options);
