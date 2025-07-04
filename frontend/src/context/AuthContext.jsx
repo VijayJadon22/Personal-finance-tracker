10d2d36a9ccd1372ef1f3f8dee4ba287c54f44c0
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-04 14:03:55 +0530

    Added toasts in AuthContext for better notifications

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,6 +3,7 @@ import { createContext } from "react";
 import axios from "../lib/axios.js";
 import { useState } from "react";
 import { useEffect } from "react";
+import toast from "react-hot-toast";
 
 const AuthContext = createContext();
 
@@ -28,6 +29,48 @@ export const AuthProvider = ({ children }) => {
     fetchUser();
   }, []);
 
-  const value = { user, loading };
+  const signupUser = async (formData) => {
+    setLoading(true);
+    const toastId = toast.loading("Creating account...");
+    try {
+      const response = await axios.post("/auth/signup", formData);
+      setUser(response.data.user);
+      toast.success("Account created successfully!",{id:"signup"});
+    } catch (error) {
+      setUser(null);
+      toast.error(error.response?.data.message || "Signup Failed");
+      return { success: false };
+    } finally {
+      toast.dismiss(toastId);
+      setLoading(false);
+    }
+  };
+  const loginUser = async (formData) => {
+    setLoading(true);
+    const toastId = toast.loading("Logging in...");
+    try {
+      const response = await axios.post("/auth/login", formData);
+      setUser(response.data.user);
+      toast.success("Logged in successfully!",{id:"login"});
+    } catch (error) {
+      setUser(null);
+      toast.error(error.response?.data.message || "Login Failed");
+    } finally {
+      toast.dismiss(toastId);
+      setLoading(false);
+    }
+  };
+  const logoutUser = async () => {
+    try {
+      await axios.post("/auth/logout", {}, { withCredentials: true });
+      setUser(null);
+      toast.success("Logged out!");
+    } catch (error) {
+      setUser(null);
+      toast.error("Logout failed");
+    }
+  };
+
+  const value = { user, loading, signupUser, loginUser, logoutUser };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
