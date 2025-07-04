92a05793ff58da5da97dd760ad3c2e7ca5dcfb92
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-04 16:26:27 +0530

    Made change for deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://personal-finance-tracker-liard.vercel.app" || "http://localhost:5173", 
   credentials: true,
 }));
 app.use(cookieParser());
